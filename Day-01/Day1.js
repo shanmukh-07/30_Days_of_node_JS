41d860d61eda52281dd2681a7f27d7f6fe82c736
shanmukh-07 <21mh1a04l4@acoe.edu.in>
2024-02-01 10:29:58 +0530

    Day-01

---
+++ Day-01/Day1.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises
+async function readFileContent(filepath){
+    try{
+        const data = await fs.readFile(filepath,'utf8');
+        console.log('File Content:- ');
+        console.log(data);
+    }catch(err){
+        console.log('Error reading File: ,${err}');
+    }
+}
+
+
+readFileContent('test_files/Text_Content.txt');
+readFileContent('test_files/Empty_File.txt');
+readFileContent('test_files/No_File.txt');
